2e17322e95fab8c15c3b3691173963371625002d
Emily Vazquez <emily.m.vazquez99@gmail.com>
2022-05-19 13:25:44 -0500

    styling changes

--- public/javascript/login.js
+++ public/javascript/login.js
@@ -8,13 +8,17 @@ const loginFormHandler = async (event) => {
   if (email && password) {
     // Send a POST request to the API endpoint
     const response = await fetch("/api/users/login", {
-      method: "POST",
-      body: JSON.stringify({ email, password }),
+      method: "post",
+      body: JSON.stringify({
+        email,
+        password,
+      }),
       headers: { "Content-Type": "application/json" },
     });
 
     if (response.ok) {
       // If successful, redirect the browser to the profile page
+
       document.location.replace("/dashboard");
     } else {
       alert(response.statusText);
